fe33de944e7e7fe85895197b8c854a65f6f9768a
Dmitrii <dimana1507@gmail.com>
2019-01-06 15:25:13 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,17 +5,22 @@ var playedApp = {
         charHintPirate: document.getElementById('ch_pirate__block'),
         charHintOverlay: document.getElementById('ch_overlay'),
         barrelBtn: document.getElementById('barrel__btn'),
+        barrelBtnOverClick: document.getElementById('barrel_clickable_btn'),
         wheel: document.getElementById('wheel'),
+        machine: document.getElementById('machine'),
+        attack_btn: document.getElementsByClassName('attack_btn'),
+        btnClickable: false,
         blickIntervalId: null,
         screenScale: null,
         stepNumber: 0,
         userScore: 4500000,
+        cannon_fire: null,
         attackBtnsInterval: null
     },
     initElements: function () {
         var self = this;
-        this.opt.barrelBtn.addEventListener('click', this.spinWheel.bind(this))
-        var attk_btn = document.getElementsByClassName('attack_btn');
+        this.opt.barrelBtnOverClick.addEventListener('click', this.spinWheel.bind(this))
+        var attk_btn = this.opt.attack_btn
         for (var i = 0; i < attk_btn.length; i++) {
             attk_btn[i].addEventListener('click', function (event) {
                 self.attack(this)
@@ -23,37 +28,49 @@ var playedApp = {
         }
         window.addEventListener("resize", this.resizeListner.bind(this))
     },
+    initSprites: function () {
+        this.opt.cannon_fire = new Image()
+        this.opt.cannon_fire.src = 'img/sprite_cannon.png';
+
+        this.opt.explr_canvas = new Image()
+        this.opt.explr_canvas.src = 'img/exp_sprite.png';
+    },
     spinWheel: function () {
-        clearInterval(this.opt.blickIntervalId);
-        var btnClickAnim = anime({
-            targets: '#barrel__btn',
-            translateY: 10,
-            delay: 50,
-            duration: 100,
-            direction: 'alternate',
-            easing: 'easeInOutQuad'
-        });
-        this.chPirateHide();
-        switch (this.opt.stepNumber) {
-            case 0:
-                this.setGlos('10');
-                this.rotateWheel(1152);
-                break;
-            case 1:
-                this.setGlos('5');
-                this.rotateWheel(-1);
-                break;
-            case 2:
-                this.setGlos('0');
-                this.rotateWheel(902);
-                break;
+        if (this.opt.btnClickable) {
+            this.opt.barrelBtn.classList.remove('hightlight__btn')
+            clearInterval(this.opt.blickIntervalId);
+            var btnClickAnim = anime({
+                targets: '#barrel__btn',
+                translateY: 10,
+                delay: 50,
+                duration: 100,
+                direction: 'alternate',
+                easing: 'easeInOutQuad'
+            });
+            this.chPirateHide();
+            switch (this.opt.stepNumber) {
+                case 0:
+                    this.setGlos('10');
+                    this.rotateWheel(1152);
+                    break;
+                case 1:
+                    this.setGlos('5');
+                    this.rotateWheel(2159);
+                    break;
+                case 2:
+                    this.setGlos('0');
+                    this.rotateWheel(3061);
+                    break;
+            }
+            this.opt.btnClickable = false;
         }
+
     },
     wheelShow: function () {
-        document.getElementById('machine').style.left = '50%'
+        this.opt.machine.style.left = '50%'
     },
     wheelHide: function () {
-        document.getElementById('machine').style.left = '-50%'
+        this.opt.machine.style.left = '-50%'
     },
     chPirateShow: function () {
         var self = this;
@@ -74,15 +91,16 @@ var playedApp = {
                 targets: '#spin__btn',
                 bottom: '0',
                 delay: 100,
-                easing: 'easeInOutQuad'
+                easing: 'easeInOutQuad',
+                complete: function() {
+                    self.btnBlick();
+                }
             }
         ]
         animeArr.forEach(function (element) {
             anime(element)
         })
-        setTimeout(function () {
-            self.btnBlick();
-        }, 900)
+
     },
     chPirateHide: function () {
         var self = this;
@@ -93,20 +111,26 @@ var playedApp = {
                 opacity: '0',
                 delay: delay,
                 easing: easing
-            },
-            {
+            },  {
                 targets: '#spin__btn',
                 bottom: '-50%',
                 delay: delay,
                 easing: easing
+            }, {
+                targets: '#ch_pirate__block',
+                left: [{
+                    value: '55%',
+                    duration: 0
+                }, {
+                    value: '155%',
+                    duration: 150,
+                    delay: 50
+                }]
             }
         ]
         animeArr.forEach(function (element) {
             anime(element)
         })
-        setTimeout(function () {
-            self.opt.charHintPirate.style.left = '155%'
-        }, 200)
     },
     winAttackMessageShow: function () {
         var obj = [{
@@ -178,78 +202,86 @@ var playedApp = {
         this.canonFire();
         this.animateAttackBtns();
     },
-    getRandomDirect : function (e, t) {
-        var n = e - .5 + Math.random() * (t - e + 1);
-        return n = Math.round(n)
+    getRandomDirect: function (min, max) {
+        var number = min - .5 + Math.random() * (max - min + 1);
+        return number = Math.round(number)
     },
     coinsSalute: function () {
         var self = this;
         var coins = document.querySelectorAll('#win_coins div');
-        var scale = this.getActualScale()
-        var coinsCounter = 0
-        self.rotateCoin(coins[coinsCounter])
-        var coinsIntervsl = setInterval(function () {
-            var rotation = self.getRandomDirect(0, 360)
-            anime({
-                targets: coins[coinsCounter],
-                scale: [{
-                    value: 0,
-                    duration: 0
-                }, {
-                    value: scale * 0.8,
-                    duration: 600
-                }],
-                top: [
-                    {
-                        value: '30%',
-                        duration: 0
-                    },
-                    {
-                        value: self.getRandomDirect(-200, 200) + "%",
-                        duration: 600,
-                        easing: "easeOutCubic"
-                    }
-                ],
-                left: [
-                    {
-                        value: '25%',
-                        duration: 0
-                    },
-                    {
-                        value: self.getRandomDirect(-200, 200) + "%",
-                        duration: 600,
-                        easing: "easeOutCubic"
-                    }
-                ],
-                rotate: [{
-                    value: rotation + "deg",
-                    duration: 0
-                }],
-                opacity: [
-                    {
+        var coinsLength = coins.length;
+        var scale = this.getActualScale();
+        var coinsCounter = 0;
+        var coinsInterval = setInterval(function () {
+            if (coinsCounter < coinsLength) {
+                var rotation = self.getRandomDirect(0, 360);
+                self.rotateCoin(coins[coinsCounter]);
+                anime({
+                    targets: coins[coinsCounter],
+                    delay: self.getRandomDirect(0, 300),
+                    scale: [{
                         value: 0,
-                        duration: 0
-                    },
-                    {
-                        value: 1,
-                        duration: 50,
-                    },
-                    {
+                        duration: 0,
+                    }, {
+                        value: scale * 0.8,
+                        duration: 200,
+                        easing: 'easeInOutQuad',
+                    }, {
                         value: 0,
-                        duration: 10,
-                        delay: 590
-                    }
-                ]
-            });
-            coinsCounter++;
-        }, 60)
-        setTimeout(function () {
-            clearInterval(coinsCounter)
-        }, 2000)
+                        delay: 400,
+                        duration: 200,
+                        easing: 'easeInOutQuad',
+                    }],
+                    top: [
+                        {
+                            value: '30%',
+                            duration: 0
+                        },
+                        {
+                            value: self.getRandomDirect(-200, 200) + "%",
+                            duration: 800,
+                            easing: "easeInOutQuad"
+                        }
+                    ],
+                    left: [
+                        {
+                            value: '50%',
+                            duration: 0
+                        },
+                        {
+                            value: self.getRandomDirect(-200, 200) + "%",
+                            duration: 800,
+                            easing: "easeInOutQuad"
+                        }
+                    ],
+                    rotate: [{
+                        value: rotation + "deg",
+                        duration: 0
+                    }],
+                    opacity: [
+                        {
+                            value: 0,
+                            duration: 0
+                        },
+                        {
+                            value: 1,
+                            duration: 50,
+                        },
+                        {
+                            value: 0,
+                            duration: 200,
+                            easing: 'easeInOutQuad',
+                            delay: 600
+                        }
+                    ]
+                });
+                coinsCounter++;
+            } else clearInterval(coinsInterval);
+        }, 16);
     },
     rotateCoin: function (elem) {
-        var position = 5; //start position for the image slicer
-        var interval = 50; //100 ms of interval for the setInterval()
+        var position = 5;
+        var interval = 50;
         var tID = setInterval(function () {
             elem.style.backgroundPosition =
                 '-' + position + 'px 0px';
@@ -285,7 +317,6 @@ var playedApp = {
     setGlos: function (attr) {
         setTimeout(function () {
             document.getElementById('glos').setAttribute('data-value', attr);
-            // document.getElementById('glos').classList.add(className)
         }, 600)
     },
     rotateWheel: function (rotate) {
@@ -360,7 +391,6 @@ var playedApp = {
                 var formatCount = numberWithCommas(count)
                 el.innerHTML = formatCount;
             }
-
         });
         function numberWithCommas(x) {
             var parts = x.toString().split(".");
@@ -376,20 +406,17 @@ var playedApp = {
         var self = this;
         this.opt.blickIntervalId = setInterval(function () {
             self.opt.barrelBtn.classList.toggle('hightlight__btn')
+            self.opt.btnClickable = true;
         }, 800)
     },
     canonFire: function () {
+        var self = this;
         var canvas = document.getElementById('canon_fire')
         var ctx = canvas.getContext('2d');
 
         var step = 0, counter = 0;
-        var sprite = new Image()
-        sprite.src = 'img/sprite_cannon.png';
-        sprite.onload = function () {
-            // draw()
-            burn();
-            requestAnimationFrame(burn)
-        };
+
+        burn();
 
         function burn() {
             if (counter > 3) {
@@ -402,7 +429,7 @@ var playedApp = {
         function draw() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             step = (step === 900 ? 0 : step + 100);
-            ctx.drawImage(sprite, step, 0, 100, 100, 0, 0, 100, 100);
+            ctx.drawImage(self.opt.cannon_fire, step, 0, 100, 100, 0, 0, 100, 100);
         }
     },
 
@@ -558,7 +585,7 @@ var playedApp = {
     attack: function (e) {
         var self = this;
         clearInterval(this.opt.attackBtnsInterval)
-        var attk_btn = document.getElementsByClassName('attack_btn');
+        var attk_btn = this.opt.attack_btn;
         for (var i = 0; i < attk_btn.length; i++) {
             if (attk_btn[i] != e) {
                 attk_btn[i].style.opacity = 0
@@ -633,7 +660,7 @@ var playedApp = {
                     document.getElementById('pet').classList.add('pet_damaged')
                     break;
             }
-        }, 1600)
+        }, 1550)
 
         var self = this;
 
@@ -657,14 +684,12 @@ var playedApp = {
     },
     drawExpl: function () {
         var canvas = document.getElementById('explr_canvas')
+        var self = this;
         var ctx = canvas.getContext('2d');
-        var sprite = new Image()
         var stepX = 0, stepY = 0, counter = 0;
-        sprite.src = 'img/exp_sprite.png';
+
         var animation = null;
-        sprite.onload = function () {
-            burn();
-        };
+        burn();
 
         function burn() {
             if (counter > 4) {
@@ -677,7 +702,7 @@ var playedApp = {
         function draw() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             if (stepX <= 6552) {
-                ctx.drawImage(sprite, stepX, 0, 468, 464, 0, 0, 468, 464);
+                ctx.drawImage(self.opt.explr_canvas, stepX, 0, 468, 464, 0, 0, 468, 464);
                 stepX += 468;
             } else {
                 cancelAnimationFrame(animation)
@@ -753,18 +778,16 @@ var playedApp = {
                 duration: 400,
                 delay: 3800
             }]
-            // scale : scale
         })
         setTimeout(function () {
             self.spinDrops()
         }, 1750)
-        // this.spinDrops()
     },
     spinDrops: function () {
         var self = this;
         var drops = document.getElementsByClassName('spin_drop')
         var counter = 0;
-        var testInterval = setInterval(function () {
+        var spinDropsInterval = setInterval(function () {
             anime({
                 targets: drops[counter],
                 top: [{
@@ -818,16 +841,18 @@ var playedApp = {
         }, 4200)
 
         setTimeout(function () {
-            clearInterval(testInterval)
+            clearInterval(spinDropsInterval)
         }, 2000)
     },
     hideLogo: function () {
-        document.getElementById('logo').style.opacity = 0
-        document.getElementById('logo').style.display = 'none';
+        var logo = document.getElementById('logo')
+        logo.style.opacity = 0
+        logo.style.display = 'none';
     },
     hideScore: function () {
-        document.getElementById('score__bar').style.opacity = 0;
-        document.getElementById('score__bar').style.display = 'none';
+        var scoreBar = document.getElementById('score__bar')
+        scoreBar.style.opacity = 0;
+        scoreBar.style.display = 'none';
     },
     finalSceen: function () {
         var scale = this.getActualScale();
@@ -876,10 +901,10 @@ var playedApp = {
         anime({
             targets: '#fp_install__btn',
             top: [{
-                value: '155%',
+                value: '179%',
                 duration: 0
             }, {
-                value: '55%',
+                value: '79%',
                 duration: 300,
                 delay: 800,
                 easing: 'easeOutExpo'
@@ -911,7 +936,7 @@ var playedApp = {
                 value: '145%',
                 duration: 0
             }, {
-                value: '45%',
+                value: '40%',
                 duration: 300,
                 delay: 600,
                 easing: 'easeOutExpo'
@@ -938,25 +963,32 @@ var playedApp = {
             tablet: false
         }
     },
-    getType: function (e) {
-        return typeof e
+    getType: function (element) {
+        return typeof element;
     },
-    setScaleForItems: function (e, i) {
+    setScaleForItems: function (elemetsArray, screenScale) {
         var self = this;
-        0 < e.length && e.map(function (e) {
-            var t = i.tabletScale;
-            if ("object" !== (void 0 === e ? "undefined" : self.getType(e))) i.tablet = true, document.getElementById(e).style.transform = "scale(" + i.tabletScale + ")";
-            else {
-                var n = e.scale ? t * e.scale : t;
-                if (e.items && 0 < e.items.length) e.items.forEach(function (e) {
-                    e.style.transform = "scale(" + n + ")"
-                });
-                else e.items.style.transform = "scale(" + n + ")"
-            }
-        })
+        if (elemetsArray.length) {
+            elemetsArray.map(function (element) {
+                var tabletScale = screenScale.tabletScale;
+                if ("object" !== (void 0 === element ? "undefined" : self.getType(element))) {
+                    screenScale.tablet = true;
+                    document.getElementById(element).style.transform = "scale(" + screenScale.tabletScale + ")";
+                }
+                else {
+                    var scale = element.scale ? tabletScale * element.scale : tabletScale;
+                    if (element.items && 0 < element.items.length) element.items.forEach(function (element) {
+                        element.style.transform = "scale(" + scale + ")"
+                    });
+                    else element.items.style.transform = "scale(" + scale + ")"
+                }
+            })
+        }
+
     },
     startAd: function () {
         this.initElements();
+        this.initSprites()
         this.opt.screenScale = this.getPageScale(640, window.innerWidth, window.innerHeight);
         this.setScaleForItems(this.opt.scaleElemArr, this.opt.screenScale);
         this.chPirateShow()
