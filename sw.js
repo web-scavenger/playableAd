14b07466f31ec67b98bfae52d982cae4aad28c31
Dmitrii <dimana1507@gmail.com>
2019-01-10 22:03:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 'use strict';
 
-importScripts('sw-toolbox.js');
+importScripts('./sw-toolbox.js');
 
-toolbox.precache(["index.html","css/style.css"]);
+toolbox.precache(["./index.html","./css/style.css"]);
 
-toolbox.router.get('/img/*', toolbox.cacheFirst);
+toolbox.router.get('./img/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
